a0a6618e1d4f49f120a7de86bdf9af42967f8da6
yannn912 <muhammad7135@gmail.com>
2026-01-05 14:28:21 +0700

    feat: add the theme

--- packages/ui/src/components/sidebar.tsx
+++ packages/ui/src/components/sidebar.tsx
@@ -24,7 +24,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { PanelLeftIcon } from "lucide-react";
 import * as React from "react";
 
-const SIDEBAR_COOKIE_NAME = "sidebar_state";
+ export const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
 const SIDEBAR_WIDTH = "16rem";
 const SIDEBAR_WIDTH_MOBILE = "18rem";
