267194acebce2b86fa482436a649de6012198f67
yannn912 <muhammad7135@gmail.com>
2026-03-30 11:38:39 +0700

    16: generating embeddings

--- packages/backend/convex/convex.config.ts
+++ packages/backend/convex/convex.config.ts
@@ -1,7 +1,8 @@
  import { defineApp } from "convex/server";
 import agent from "@convex-dev/agent/convex.config";
+import rag from "@convex-dev/rag/convex.config";
 
 const app = defineApp();
 app.use(agent);
-
+app.use(rag);
 export default app;
